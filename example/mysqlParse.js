fde77a24bbc598d9c2e47639e567497b31c598cc
Nadim Tuhin <nadimtuhin@gmail.com>
2020-02-02 23:34:08 +0600

    Update mysqlParse.js

--- example/mysqlParse.js
+++ example/mysqlParse.js
@@ -1,6 +1,6 @@
 const parser = require('../parsers/mysql/parser');
 
-const sql = require('fs').readFileSync('../data/corp.sql', 'utf-8');
+const sql = require('fs').readFileSync('./db.sql', 'utf-8');
 const ast = parser.parse(sql);
 
 console.log(JSON.stringify(ast, null, 2));
